8557ceb0ca28a7a3e509b90db95c596c37e8c0cf
Antony Culqui <a.culqui02@gmail.com>
2025-11-07 10:51:56 -0500

    fix expenses

--- resources/views/livewire/cash/general-report.blade.php
+++ resources/views/livewire/cash/general-report.blade.php
@@ -10,9 +10,6 @@
             font-size: 10px !important;
             text-align: center !important;
             vertical-align: middle; /* <-- clave */
-            white-space: normal !important;
-            word-break: break-word;
-            overflow-wrap: anywhere;
         }
 
         .btn, input, select {
